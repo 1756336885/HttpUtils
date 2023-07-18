b600147252e3371a220f1553c0fcc6c41884b67c
NewName <1756336885@qq.com>
2023-07-18 21:35:06 +0800

    ini

---
+++ HttpUtils.cpp
@@ -0,0 +1,39 @@
+﻿// HttpUtils.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "HttpUtils.h"
+#include"httplib.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// 这是导出变量的一个示例
+HTTPUTILS_API int nHttpUtils=0;
+
+// 这是导出函数的一个示例。
+HTTPUTILS_API int fnHttpUtils(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+CHttpUtils::CHttpUtils()
+{
+    return;
+}
+
+HTTPUTILS_API void testHttp() {
+    httplib::Client cli("www.baidu.com");
+    auto res = cli.Post("/");
+
+    string str;
+
+    std::cout<< str << res->body << std::endl;
+
+
+    printf("port :%s\n", str.data());
+    
+}
---
+++ HttpUtils.h
@@ -0,0 +1,27 @@
+﻿// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 HTTPUTILS_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// HTTPUTILS_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef HTTPUTILS_EXPORTS
+#define HTTPUTILS_API __declspec(dllexport)
+#else
+#define HTTPUTILS_API __declspec(dllimport)
+#endif
+
+// 此类是从 dll 导出的
+class HTTPUTILS_API CHttpUtils {
+public:
+	CHttpUtils(void);
+	// TODO: 在此处添加方法。
+};
+
+extern HTTPUTILS_API int nHttpUtils;
+
+HTTPUTILS_API int fnHttpUtils(void);
+
+
+extern "C"  HTTPUTILS_API void testHttp();
+
+
